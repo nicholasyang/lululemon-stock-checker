2463aeaf25d9873f869adcb0069f15207469e1be
Nicholas Yang <nicholasjyang@pm.me>
2022-07-18 13:53:21 -0400

    use boto3 resources

--- handler.py
+++ handler.py
@@ -6,22 +6,22 @@ TABLE = "lululemon-stock-checker-items"
 SNS_TOPIC = "arn:aws:sns:us-east-1:259356154792:lululemon-stock-checker-notification"
 
 def run(event, context):
-	dynamo_client = boto3.client("dynamodb")
-	items = dynamo_client.scan(
-		TableName=TABLE
-	)
+	dynamo_client = boto3.resource("dynamodb")
+	table = dynamo_client.Table(TABLE)
+	table_scan = table.scan()
+	items = table_scan["Items"]
 
-	for item in items["Items"]:
-		response = requests.get(item["url"]["S"])
+	for item in items:
+		response = requests.get(item["url"])
 		soup = BeautifulSoup(response.text, features="html.parser")
 
 		if soup.find("div", string="Sold out online.") == None:
 			sns_client = boto3.client("sns")
 			sns_client.publish(
 				TopicArn=SNS_TOPIC,
-				Subject=f'{item["name"]["S"]} in stock!',
-				Message=f'Link: {item["url"]["S"]}'
+				Subject=f'{item["name"]} in stock!',
+				Message=f'Link: {item["url"]}'
 			)
-			print(f'{item["name"]["S"]} in stock! :)')
+			print(f'{item["name"]} in stock! :)')
 		else:
-			print(f'{item["name"]["S"]} not in stock :(')
+			print(f'{item["name"]} not in stock :(')
